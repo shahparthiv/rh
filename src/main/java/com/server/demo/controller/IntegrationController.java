982c39c02f033e775ca283b8fba452fef687e4b2
Parthiv Shah <parthivshah@Parthivs-MacBook-Air.local>
2024-12-14 15:36:42 +0530

    Rename the controler and endpoint

--- src/main/java/com/server/demo/controller/IntegrationController.java
+++ src/main/java/com/server/demo/controller/IntegrationController.java
@@ -13,25 +13,29 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
-public class YAMLController {
-    private static final Logger logger = LoggerFactory.getLogger(YAMLController.class);
+public class IntegrationController {
+    private static final Logger logger = LoggerFactory.getLogger(IntegrationController.class);
 
     private String loadYamlConfig(String resourcePath) throws IOException {
-        InputStream inputStream = YAMLController.class.getClassLoader().getResourceAsStream(resourcePath);
+        InputStream inputStream = IntegrationController.class.getClassLoader().getResourceAsStream(resourcePath);
         if (inputStream == null) {
             throw new IllegalArgumentException("YAML configuration file not found: " + resourcePath);
         }
         return new String(inputStream.readAllBytes());
     }
 
-    @GetMapping("/rh/submit")
+    @GetMapping("/runIntegrarion")
     public String rh() throws IOException {
 
 
         logger.info("Hello {}","RH");
         // Make an HTTP call to downstream service with the custom headers
 
+        //We can have a code get the file from client but as of now I have added the yaml file in resource directory.
+        // We can write logic to validate the yml file. In this type of feature validation is very crucial task as
+        // the whole integration is dependent on it.
         String ymlConfig = loadYamlConfig("rh/cs.yml");
+
 
         WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
         WorkflowClient client = WorkflowClient.newInstance(service);
