1b5351b648dfae6acb655ce60d18bbec6a4a88e5
Parthiv Shah <parthivshah@Parthivs-MacBook-Air.local>
2024-12-16 15:19:16 +0530

    Added activity and workflow exception handelling

--- src/main/java/com/temoral_worker/integration/core/IntegrationActivitiesImpl.java
+++ src/main/java/com/temoral_worker/integration/core/IntegrationActivitiesImpl.java
@@ -5,19 +5,27 @@ import com.temoral_worker.integration.step.Step;
 import io.temporal.activity.Activity;
 import io.temporal.activity.DynamicActivity;
 import io.temporal.common.converter.EncodedValues;
+import io.temporal.failure.ApplicationFailure;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class IntegrationActivitiesImpl implements DynamicActivity {
 
     @Override
     public Map<String, Object> execute(EncodedValues args) {
-        Map<String, Object> stepDetails = args.get(0, Map.class);
-        Map<String, Object> context = args.get(1, Map.class);
-        String stepType = Activity.getExecutionContext().getInfo().getActivityType();
-        Step step = StepFactory.getHandler(stepType);
+        Map<String, Object> returnContext = new HashMap<>();
+        try {
+            // Activity logic
+            Map<String, Object> stepDetails = args.get(0, Map.class);
+            Map<String, Object> context = args.get(1, Map.class);
+            String stepType = Activity.getExecutionContext().getInfo().getActivityType();
+            Step step = StepFactory.getHandler(stepType);
 
-        Map<String, Object> returnContext = step.execute(stepDetails, context);
+            returnContext = step.execute(stepDetails, context);
+        } catch (Exception e) {
+            throw ApplicationFailure.newFailure("General error occurred", "GeneralErrorType", e);
+        }
 
         return returnContext;
     }
--- src/main/java/com/temoral_worker/integration/core/IntegrationWorkflowImpl.java
+++ src/main/java/com/temoral_worker/integration/core/IntegrationWorkflowImpl.java
@@ -2,7 +2,9 @@ package com.temoral_worker.integration.core;
 
 import com.temoral_worker.integration.step.AuthenticationStep;
 import io.temporal.activity.ActivityOptions;
+import io.temporal.common.RetryOptions;
 import io.temporal.common.converter.EncodedValues;
+import io.temporal.failure.ApplicationFailure;
 import io.temporal.workflow.ActivityStub;
 import io.temporal.workflow.Workflow;
 import org.slf4j.Logger;
@@ -20,27 +22,43 @@ public class IntegrationWorkflowImpl implements IntegrationWorkflow {
     private static final Logger logger = LoggerFactory.getLogger(IntegrationWorkflowImpl.class);
     private final ActivityOptions options = ActivityOptions.newBuilder()
             .setStartToCloseTimeout(Duration.ofMinutes(1))
+            // We can set the proper integrations tep retry logic
+            /*.setRetryOptions(RetryOptions.newBuilder()
+                    .setMaximumAttempts(3)
+                    .setInitialInterval(Duration.ofSeconds(2))
+                    .setMaximumInterval(Duration.ofMinutes(1))
+                    .setDoNotRetry("SpecificErrorType") // Non-retryable exception
+                    .build())*/
             .build();
 
     @Override
     public Object executeIntegration(String yamlConfig) {
-        Yaml yaml = new Yaml();
-        Map<String, Object> config = yaml.load(yamlConfig);
         Map<String, Object> context = new HashMap<>();
+        try {
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(yamlConfig);
 
 
-        // Extract steps from YAML
+            // Extract steps from YAML
 
-        List<Map<String, Object>> steps = (List<Map<String, Object>>) config.get("steps");
+            List<Map<String, Object>> steps = (List<Map<String, Object>>) config.get("steps");
 
-        // Execute each step
-        for (Map<String, Object> step : steps) {
-            String stepType = (String) step.get("step_type");
-            ActivityStub ac = Workflow.newUntypedActivityStub(options);
-            //ac.execute(stepType, Map.class, step.get("details"));
-            context = ac.execute(stepType, Map.class, step.get("details"), context);
+            // Execute each step
+            for (Map<String, Object> step : steps) {
+                String stepType = (String) step.get("step_type");
+                ActivityStub ac = Workflow.newUntypedActivityStub(options);
+                //ac.execute(stepType, Map.class, step.get("details"));
+                context = ac.execute(stepType, Map.class, step.get("details"), context);
+            }
+
+
+        } catch (ApplicationFailure e) {
+            if ("SpecificErrorType".equals(e.getType())) {
+                // Handle specific error
+            } else {
+                // Handle general errors
+            }
         }
-
         return context;
     }
 }
